482a703f830746dad457e818cb69edd48a00c36f
felipereyel <felipe.reyel@gmail.com>
2025-08-23 22:25:14 -0300

    sync + ui adjustments

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -5,28 +5,57 @@
 #define FONT_SIZE_SMALL 1
 #define FONT_SIZE_MEDIUM 2
 #define FONT_SIZE_LARGE 3
+#define FONT_SIZE_XLARGE 4
 #define PROGRESS_BAR_MARGIN 10
 #define PROGRESS_BAR_HEIGHT 10
 #define SECRET_SEPARATOR ':'
 #define MAX_KEYS 5
+#define STEP 30
+#define DIGITS 6
 
 // Global Variables
 
-u_int32_t last_time = 0;
-
-// Secrets Storage
+time_t ltime = 0;
+time_t time_delta;
 
 Preferences preferences;
 String current_provider;
 String current_seed;
 u8_t current_index;
+
+//// computed
 String current_key;
+String current_totp;
+
+// TOTP
+
+time_t now()
+{
+    return time(NULL) + time_delta;
+}
+
+void compute_totp()
+{
+    if (current_seed.isEmpty())
+    {
+        current_totp.clear();
+        return;
+    }
+
+    String *otp = TOTP::currentOTP(current_seed, ltime, STEP, DIGITS, 0);
+    current_totp = *otp;
+    delete otp;
+}
+
+// Secrets Storage
 
 bool sync_index_key_secret()
 {
     current_provider.clear();
     current_seed.clear();
+    current_totp.clear();
     current_key.clear();
+
     current_key.concat("key-");
     current_key.concat(String(current_index));
 
@@ -71,13 +100,20 @@ void put_secret(String raw_secret)
 
 // Grafics
 
-void draw_progress_bar(int current, int total)
+void draw_totp_screen()
 {
+    if (current_seed.isEmpty() || current_provider.isEmpty() || current_totp.isEmpty())
+    {
+        return;
+    }
+
+    int current = (ltime) % STEP;
+    float progress = (float)current / STEP;
+
     int width = M5.Display.width();
     int height = M5.Display.height();
     int bar_height = PROGRESS_BAR_HEIGHT;
     int bar_width = width - 2 * PROGRESS_BAR_MARGIN;
-    float progress = (float)current / total;
 
     int bg_color = M5.Display.color565(200, 200, 200);
     int bar_color = M5.Display.color565(0, 255, 0);
@@ -91,8 +127,12 @@ void draw_progress_bar(int current, int total)
     }
 
     M5.Display.fillRect(10, height - bar_height - 10, bar_width, bar_height, bg_color);
-    int filled_width = (current * bar_width) / total;
+    int filled_width = (current * bar_width) / STEP;
     M5.Display.fillRect(10, height - bar_height - 10, filled_width, bar_height, bar_color);
+
+    M5.Display.setTextSize(FONT_SIZE_XLARGE);
+    M5.Display.setCursor(85, 100);
+    M5.Display.printf("%s\n", current_totp.c_str());
 }
 
 void draw_text_ui()
@@ -100,8 +140,8 @@ void draw_text_ui()
     M5.Display.clear();
     M5.Display.setCursor(0, 0);
 
-    M5.Display.setTextSize(FONT_SIZE_SMALL);
-    M5.Display.printf("slot %d/%d\n\n", current_index + 1, MAX_KEYS);
+    M5.Display.setTextSize(FONT_SIZE_MEDIUM);
+    M5.Display.printf("slot %d/%d: ", current_index + 1, MAX_KEYS);
 
     auto isEmpty = current_provider.isEmpty() || current_seed.isEmpty();
     auto slotname = isEmpty ? "EMPTY" : current_provider;
@@ -109,27 +149,11 @@ void draw_text_ui()
     M5.Display.printf("%s\n", slotname);
 }
 
-void draw_totp()
-{
-    if (current_seed.isEmpty())
-    {
-        return;
-    }
-
-    String *otp = TOTP::currentOTP(current_seed);
-    if (otp)
-    {
-        M5.Display.setTextSize(FONT_SIZE_LARGE);
-        M5.Display.setCursor(0, 50);
-        M5.Display.printf("%s\n", *otp);
-    }
-}
-
 // Arduino Setup and Loop
 
 void setup()
 {
-    last_time = millis();
+    ltime = now();
     auto cfg = M5.config();
     cfg.serial_baudrate = 115200;
     M5.begin(cfg);
@@ -141,7 +165,6 @@ void setup()
 
     // Draw initial UI
     M5.Display.init();
-    M5.Display.startWrite();
     M5.Display.setBrightness(100);
     M5.Display.fillScreen(TFT_BLACK);
     draw_text_ui();
@@ -151,13 +174,20 @@ void setup()
 
 void loop()
 {
-    auto time = millis();
+    time_t ctime = now();
 
     if (Serial.available())
     {
         Serial.println("Serial Input Detected");
         String input = Serial.readStringUntil('\n');
-        if (input.startsWith("set "))
+
+        if (input.startsWith("sync "))
+        {
+            input.remove(0, 5);
+            input.trim();
+            time_delta = (time_t)input.toInt() - time(NULL);
+        }
+        else if (input.startsWith("set "))
         {
             input.remove(0, 4);
             input.trim();
@@ -182,33 +212,26 @@ void loop()
 
     if (M5.BtnA.isHolding())
     {
-        if (current_seed.isEmpty())
+        if (current_totp.isEmpty())
         {
             return;
         }
 
-        String *otp = TOTP::currentOTP(current_seed);
-        if (!otp)
-        {
-            return;
-        }
-
+        Serial.printf("%s: %s\n", current_provider.c_str(), current_totp.c_str());
         while (M5.BtnA.isHolding())
         {
-            delay(10);
+            M5.delay(10);
             M5.update();
         }
     }
 
-    if (time - last_time > 1000)
+    if (ctime > ltime)
     {
-        last_time = time;
-        int progress = (time / 1000) % 30;
-        draw_totp();
-        draw_progress_bar(progress, 30);
+        ltime = ctime;
+        compute_totp();
+        draw_totp_screen();
     }
 
     M5.delay(10);
     M5.update();
-    M5.Display.endWrite();
 }
